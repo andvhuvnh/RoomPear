31cff9db141e61d8fa4fd861e99ea43cddcc75ee
Andvhuvnh <ajhuynh23@gmail.com>
2026-05-10 00:49:53 -0700

    eas build works and ios notification popup succeeds

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -18,6 +18,9 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.ajhuynh.roompear',
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
     },
     android: {
       package: 'com.ajhuynh.roompear',
@@ -32,6 +35,9 @@ module.exports = {
       favicon: './assets/favicon.png',
     },
     extra: {
+      eas: {
+        projectId: '3bd4beb6-0e38-4c19-8465-99d1fe6c79c7',
+      },
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
       mapboxAccessToken: process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN,
@@ -39,7 +45,7 @@ module.exports = {
       revenueCatIosKey: process.env.EXPO_PUBLIC_REVENUECAT_IOS_KEY,
       revenueCatAndroidKey: process.env.EXPO_PUBLIC_REVENUECAT_ANDROID_KEY,
     },
-    plugins: ['expo-font'],
+    plugins: ['expo-font', 'expo-notifications'],
   },
 };
 
